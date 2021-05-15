46515f77e9ba91a16e00638d91b2dafba5e4cc53
Arniel <aceba1@outlook.com>
2021-05-15 11:32:15 -0400

    Update setTime.js

--- util/setTime.js
+++ util/setTime.js
@@ -6,11 +6,9 @@ function leadZero(n) {
 
 module.exports = function setTime(value) {
   const date = new Date(value);
-
   console.log(`old Date is ${new Date()}`);
   execSync('sudo timedatectl set-ntp false');
   execSync(`sudo timedatectl set-time ${date.getFullYear()}-${leadZero(date.getMonth() + 1)}-${leadZero(date.getDate())}`);
   execSync(`sudo timedatectl set-time ${leadZero(date.getHours())}:${leadZero(date.getMinutes())}:${leadZero(date.getSeconds())}`);
-  execSync('sudo timedatectl set-ntp true');
   console.log(`new Date is ${new Date()}`);
 };
